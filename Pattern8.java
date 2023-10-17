45af3d39ee42d4f4b42b998bff794cee53c2ba21
Anurag Tyagi <145704269+anuragty07@users.noreply.github.com>
2023-10-17 11:45:05 +0530

    Add files via upload

---
+++ Pattern8.java
@@ -0,0 +1,28 @@
+package javanew;
+
+import java.util.Scanner;
+
+public class Pattern8 {
+    public static void main(String[] args) {
+         Scanner sc= new Scanner(System.in);
+        System.out.println("enter row");
+        int r=sc.nextInt();
+        for (int i = 1; i <= r; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" ");
+                
+            }
+            
+           for (int j = 1; j <= 6; j++) {
+            if(i==1||i==r||j==1||j==6){
+            System.out.print("*");
+            }
+            else{
+           System.out.print(" ");
+            }
+        }
+           System.out.println(); 
+    
+    }   
+}
+}
